3b3f0f6d2cc93a1005afae251bf0b35383473af9
Di Belogrivaya <diana.belogrivaya@gmail.com>
2018-03-01 11:37:32 +0300

    Bug fixes.

--- DBAnimatedTabbar/DBAnimatedTabbarItem.m
+++ DBAnimatedTabbar/DBAnimatedTabbarItem.m
@@ -91,7 +91,7 @@
     [container addConstraint:[NSLayoutConstraint constraintWithItem:icon attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:container attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-5-[textLabel]-5-|" options:NSLayoutFormatAlignAllCenterX metrics:nil views:NSDictionaryOfVariableBindings(textLabel)]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=5)-[icon(30)]-(>=5)-|" options:NSLayoutFormatAlignAllCenterX metrics:nil views:NSDictionaryOfVariableBindings(icon)]];
-    [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-8-[icon]-3-[textLabel]-3-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(icon, textLabel)]];
+    [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-3-[icon(30)]-3-[textLabel]-3-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(icon, textLabel)]];
     
     self.image = nil;
     self.title = nil;
